c7b3ccb128f3e55ad3c4be13268070a93ea1c496
Derrick <dcleavall@gmail.com>
2023-02-06 15:55:52 -0700

    done

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+return "something"
+}
+function subtract(a, b) {
+    return "something"
+}
+function multiply(a, b) {
+    return "something"
+}
+function divide(a, b) {
+    return "something"
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+number = 10;
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
